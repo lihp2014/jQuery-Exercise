42494e81ece9c06566cff1e5f2ad59b014148179
liqinglin <1317971527@qq.com>
2015-09-11 14:34:37 +0800

    add clearTimeout

--- tab/js/tab.js
+++ tab/js/tab.js
@@ -1,13 +1,16 @@
 $(function() {
 	var tLi = $('.title li');
 	var contLi = $('.content li');
+	var timeID = null;
 
 	tLi.mouseover(function() {
 		var _this = $(this);
-		setTimeout(function() {
+		timeID = setTimeout(function() {
 			var index = _this.index();
 			_this.addClass('current').siblings().removeClass('current');
 			contLi.eq(index).removeClass('hide').siblings().addClass('hide');
 		},300);
+	}).mouseout(function() {
+		clearTimeout(timeID);
 	});
 });
