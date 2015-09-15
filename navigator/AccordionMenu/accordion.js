f879a17a2bb57ca26ccb0e58459598ba9ea7b485
liqinglin <1317971527@qq.com>
2015-09-15 17:18:03 +0800

    unite the right pointer's status and the content's

--- navigator/AccordionMenu/accordion.js
+++ navigator/AccordionMenu/accordion.js
@@ -1,11 +1,18 @@
 $(function(){
 	var oTitle = $('.title');
 	oTitle.click(function(){
-		$(this).next().slideToggle().siblings('ul').slideUp();
-		if($(this).children('span').hasClass('point-right')){
-			$(this).children('span').removeClass('point-right').addClass('point-down');
-		} else {
-			$(this).children('span').removeClass('point-down').addClass('point-right');
-		}
+		$(this).next().slideToggle('fast',function(){
+			if($(this).css('display')=='block') {
+				$(this).prev().children('span').removeClass('point-right').addClass('point-down');
+			} else {
+				$(this).prev().children('span').removeClass('point-down').addClass('point-right');
+			}
+		}).siblings('ul').slideUp('fast',function(){
+			if($(this).css('display')=='block') {
+				$(this).prev().children('span').removeClass('point-right').addClass('point-down');
+			} else {
+				$(this).prev().children('span').removeClass('point-down').addClass('point-right');
+			}
+		});
 	});
 });
