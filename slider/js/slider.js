bcf6c3f3eb4cdbbc362e773f6a29eedadce76765
liqinglin <1317971527@qq.com>
2015-09-15 09:50:22 +0800

    complete picture slides without gap

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -4,23 +4,35 @@ $(function(){
 	var imgLi = $('.wrap ul li');
 	var imgWidth = imgLi.width();
 	var _now = 0;//保存数字索引
+	var _now2 = 0;//保存图片滑动索引
 	var timeId = null;
 
 	numLi.click(function(){
 		var index = $(this).index();
 		_now = index;
 		$(this).addClass('current').siblings().removeClass('current');
-		oul.animate({'left':-imgWidth*index});
+		oul.animate({'left':-imgWidth*index},500);
 	});
 
 	function slide() {
 		if(_now == numLi.length-1) {
+			imgLi.eq(0).css({
+				'position':'relative',
+				'left':oul.width()-imgWidth
+			})
 			_now = 0;
 		} else {
 			_now++;
 		}
+		_now2++;
 		numLi.eq(_now).addClass('current').siblings().removeClass('current');
-		oul.animate({'left':-imgWidth*_now});
+		oul.animate({'left':-imgWidth*_now2},500,function(){
+			if(_now==0){
+				imgLi.eq(0).css('position','static');
+				oul.css('left',0);
+				_now2=0;
+			}
+		});
 	}
 
 	timeId = setInterval(slide,1000);
