e44f330706d2396cc394b71b62e01a093c08ee8f
liqinglin <1317971527@qq.com>
2015-09-16 16:06:05 +0800

    set the pop-up box in the middle of the window

--- popWindow/js/popWindow.js
+++ popWindow/js/popWindow.js
@@ -3,8 +3,20 @@ $(function(){
 	var popWindow = $('.popWindow');
 	var closeBtn = $('.close');
 
+	var viewWidth = $(window).width();
+	var viewHeight = $(window).height();
+
+	var popWidth = $('.popWindow').outerWidth();
+	var popHeight = $('.popWindow').height();
+
+	var positionLeft = viewWidth/2 - popWidth/2;
+	var positionTop = viewHeight/2 - popHeight/2;
+
 	loginBtn.click(function(){
-		popWindow.show();
+		popWindow.show().css({
+			'left':positionLeft,
+			'top':positionTop
+		});
 	});
 	closeBtn.click(function(){
 		popWindow.hide();
