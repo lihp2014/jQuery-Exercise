9b3a2335e42f6c6858e7a2a8a5224d5f10547eb2
liqinglin <1317971527@qq.com>
2015-09-17 10:33:03 +0800

    make the inner dive only move in the outer div

--- drag/js/drag.js
+++ drag/js/drag.js
@@ -1,22 +1,23 @@
 $(function(){
 
 	$('.draggable').mousedown(function(e){
+		var outerDiv = $('.outer').offset();
 		var offsetDiv = $('.draggable').offset();
-		var disX = e.pageX - offsetDiv.left;
-		var disY = e.pageY - offsetDiv.top;
+		var disX = e.pageX - offsetDiv.left + outerDiv.left;
+		var disY = e.pageY - offsetDiv.top + outerDiv.left;
 
 		$(document).mousemove(function(e){
 			var posLeft = e.pageX - disX;
 			var posTop = e.pageY - disY;
 			if(posLeft < 0) {
 				posLeft = 0;
-			} else if (posLeft > $(document).width() - $('.draggable').outerWidth()) {
-				posLeft = $(document).width() - $('.draggable').outerWidth();
+			} else if (posLeft > $('.outer').width() - $('.draggable').outerWidth()) {
+				posLeft = $('.outer').width() - $('.draggable').outerWidth();
 			}
 			if(posTop < 0) {
 				posTop = 0;
-			} else if (posTop > $(document).height() - $('.draggable').height()) {
-				posTop = $(document).height() - $('.draggable').height();
+			} else if (posTop > $('.outer').height() - $('.draggable').height()) {
+				posTop = $('.outer').height() - $('.draggable').height();
 			}
 			$('.draggable').css({
 				'left':posLeft,
