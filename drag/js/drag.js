ee894ef899daee9d83c6caf6ad5ac6607fb8edbe
liqinglin <1317971527@qq.com>
2015-09-17 10:42:31 +0800

    add comment

--- drag/js/drag.js
+++ drag/js/drag.js
@@ -1,14 +1,15 @@
 $(function(){
 
 	$('.draggable').mousedown(function(e){
+		//offset() 是元素相对于文档的定位
 		var outerDiv = $('.outer').offset();
 		var offsetDiv = $('.draggable').offset();
-		var disX = e.pageX - offsetDiv.left + outerDiv.left;
-		var disY = e.pageY - offsetDiv.top + outerDiv.left;
+		var disX = e.pageX - offsetDiv.left;
+		var disY = e.pageY - offsetDiv.top;
 
 		$(document).mousemove(function(e){
-			var posLeft = e.pageX - disX;
-			var posTop = e.pageY - disY;
+			var posLeft = e.pageX - disX - outerDiv.left;
+			var posTop = e.pageY - disY - outerDiv.top;
 			if(posLeft < 0) {
 				posLeft = 0;
 			} else if (posLeft > $('.outer').width() - $('.draggable').outerWidth()) {
